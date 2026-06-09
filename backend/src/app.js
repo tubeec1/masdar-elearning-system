fa384c78c6337da9db61a36dd04247d5f7bd610a
Mohamed Suleyman Ibrahim <mohamedsuleyman813@gmail.com>
2026-06-09 11:07:44 +0300

    Setup backend structure and nodemon

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-let testRout = require ("./routes/testR.js")
+let testRout = require("./routes/testR.js");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/" ,testRout)
+app.use("/", testRout);
+
 module.exports = app;
-
-
-
-
-
