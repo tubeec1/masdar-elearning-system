5fe99626430ee44f4f5f4fdcbb5924d49a7c427a
Mohamed Suleyman Ibrahim <mohamedsuleyman813@gmail.com>
2026-06-13 11:25:44 +0300

    correcting response to make success flag instead of status

--- backend/src/services/categoryService.js
+++ backend/src/services/categoryService.js
@@ -1,59 +1,62 @@
 const categoryModel = require("../models/categoryModel");
-let AppError = require("../utilities/AppError")
-const createCategory = async (name ,description ,status ,thumbnail) => {
-const response=  await categoryModel.createCategory(name ,description ,status,thumbnail );
+let AppError = require("../utilities/AppError");
+const createCategory = async (name, description, status, thumbnail) => {
+  const response = await categoryModel.createCategory(
+    name,
+    description,
+    status,
+    thumbnail,
+  );
 
- return {
-   status:"true",
-   message:"successfully created",
-   category:response
- }
+  return {
+    success: "true",
+    message: "successfully created",
+    category: response,
+  };
 };
 
 const getAllCategories = async () => {
-const response = await categoryModel.getAllCategories();
-return{
-  status:"true",
-  message:"reading succefully",
-  category:response
-}
+  const response = await categoryModel.getAllCategories();
+  return {
+    success: "true",
+    message: "reading succefully",
+    category: response,
+  };
 };
 
 const getCategoryById = async (id) => {
+  const response = await categoryModel.getCategoryById(id);
 
-const response =await categoryModel.getCategoryById(id);
-
-if(!response){
-  throw new AppError("Category not found")
-}
-return{
-  status:"true",
-  message:"reading succefully",
-  category:response
-}
+  if (!response) {
+    throw new AppError("Category not found");
+  }
+  return {
+    success: "true",
+    message: "reading succefully",
+    category: response,
+  };
 };
 
 const updateCategory = async (id, name, description, thumbnail) => {
-
   const response = await categoryModel.updateCategory(
     id,
     name,
     description,
-    thumbnail
+    thumbnail,
   );
 
   return {
-    success: true,
+    success: "true",
     message: "Category updated successfully",
-    category: response
+    category: response,
   };
 };
 const deleteCategory = async (id) => {
-  const response= await categoryModel.deleteCategory(id);
-  return{
-  success: true,
-   message: "Category deleted successfully",
-  }
+  const response = await categoryModel.deleteCategory(id);
+  return {
+    success: true,
+    message: "Category deleted successfully",
+  };
 };
 
 module.exports = {
